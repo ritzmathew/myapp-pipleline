78ab3863a88cefec011856a97c22128869093041
Ritz A Mathew <ritz.mathew@capgemini.com>
2023-01-19 12:44:44 +0000

    removed variable

--- backend.tf
+++ backend.tf
@@ -1,8 +1,3 @@
-variable "bucket_name" {
-  type        = string
-  description = "The name of the Google Storage Bucket to create"
-}
-
 variable "storage_class" {
   type        = string
   description = "The storage class of the Storage Bucket to create"
