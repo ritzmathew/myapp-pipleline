e41b2ca88f2eefe43c9efb0306cd1b9cfec6318b
Ritz Mathew <mathewritz91@gmail.com>
2023-01-19 21:23:39 +0000

    added encryption

--- backend.tf
+++ backend.tf
@@ -2,5 +2,7 @@ terraform {
  backend "gcs" {
    bucket  = "my-sample-app-375112-tf-bucket"
    prefix  = "terraform/state"
+   
+   encryption_key = "RQVFmYP9tCzCipwP5Cr3skrisMQ0DMQTZhz4D2OEfuM="
  }
 }
